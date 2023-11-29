21682c24f3b85d7bd1d38f3f5ba529c0583af3a7
HEran-ite <137596848+HEran-ite@users.noreply.github.com>
2023-11-29 17:58:28 +0300

    Added solution for Convert the Temperature

---
+++ leet-code-solutions/convert-the-temperature.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        kel=float(celsius+273.15)
+        fah=float(celsius*1.80+32.00)
+        return[kel,fah]
