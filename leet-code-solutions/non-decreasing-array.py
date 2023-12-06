2e3352e88cda466bbb9aa6d31997cf1af52836ab
HEran-ite <137596848+HEran-ite@users.noreply.github.com>
2023-12-06 22:23:25 +0300

    Added solution for Non-decreasing Array

---
+++ leet-code-solutions/non-decreasing-array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        count=0
+        for i in range (len(nums)-1):
+            if nums[i]>nums[i+1]:
+                count+=1
+                if i > 0 and nums[i + 1] < nums[i - 1]:
+                    nums[i + 1] = nums[i]
+                else:
+                    nums[i] = nums[i + 1]
+
+            if count >=2:
+                return False
+        return True
