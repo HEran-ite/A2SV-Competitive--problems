81b8b885d941776bfff174ce9e3a968bc91869cb
HEran-ite <137596848+HEran-ite@users.noreply.github.com>
2024-03-03 12:41:01 +0300

    Add solution for Generate Parentheses

--- leetcode/generate-parentheses.py
+++ leetcode/generate-parentheses.py
@@ -3,16 +3,28 @@ class Solution:
         res=[]
         def backtrack(opened,closed,gen):
             nonlocal res
+        
             if opened==0 and closed==0:
-                res.append(gen[:])
+                res.append(''.join(gen[:]))
                 return 
 
-            if opened > 0:
-                pickOpened = backtrack(opened-1,closed,gen+'(')
-
-            if opened < closed:
-                pickClosed=backtrack(opened,closed-1,gen+")")
                 
+            if opened < closed:
+                gen.append(')')
+                backtrack(opened,closed-1,gen[:])
+                gen.pop()
 
-        backtrack(n,n,"")
+            if opened >0:
+                gen.append("(")
+                backtrack(opened-1,closed,gen[:])
+                gen.pop()
+
+        backtrack(n,n,[])
         return res
+
+
+ # if opened > 0:
+            #     pickOpened = backtrack(opened-1,closed,gen+'(')
+
+            # if opened < closed:
+            #     pickClosed=backtrack(opened,closed-1,gen+")")
